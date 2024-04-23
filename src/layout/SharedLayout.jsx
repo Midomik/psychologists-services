0f44e9b3eb6dfdbede2fcdcf5c95102a5238ddfe
Midomik <krastikrabbs9234@gmail.com>
2024-04-23 23:29:32 +0300

    build: implemended home page

--- src/layout/SharedLayout.jsx
+++ src/layout/SharedLayout.jsx
@@ -1,55 +1,67 @@
-import React from 'react';
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, NavLink, Outlet, useLocation } from 'react-router-dom';
 import css from './SharedLayout.module.css';
 import { BurgerMenuIcon, UserIcon } from 'assets/sprite';
 
 export const SharedLayout = () => {
+  const location = useLocation();
+  const body = document.body;
+
+  useEffect(() => {
+    location.pathname === '/'
+      ? body.classList.add('add-background')
+      : body.classList.remove('add-background');
+  }, [location.pathname, body]);
+
   return (
-    <div className="container">
+    <>
       <header>
-        <Link className={css.logo} to="/">
-          <span>psychologists</span>
-          <span>.</span>
-          <span>services</span>
-        </Link>
+        <div className={`container ${css.header_container}`}>
+          <Link className={css.logo} to="/">
+            <span>psychologists</span>
+            <span>.</span>
+            <span>services</span>
+          </Link>
 
-        <div className={css.pages_container}>
-          <NavLink className={css.nav_link} to="/">
-            Home
-          </NavLink>
-          <NavLink className={css.nav_link} to="psychologists">
-            Psychologists
-          </NavLink>
+          <div className={css.pages_container}>
+            <NavLink className={css.nav_link} to="/">
+              Home
+            </NavLink>
+            <NavLink className={css.nav_link} to="psychologists">
+              Psychologists
+            </NavLink>
 
-          <NavLink
-            className={`${css.nav_link} ${false && css.visually_hidden}`}
-            to="favorites"
-          >
-            Favorites
-          </NavLink>
-        </div>
-
-        <div className={css.auth_container}>
-          <button className={css.auth_btn}>LogIn</button>
-          <button className={css.auth_btn}>Registration</button>
-        </div>
-
-        {false && (
-          <div className={css.avatar_container}>
-            <div className={css.icon_container}>
-              <UserIcon />
-            </div>
-            <p className={css.user_title}>{'User'}</p>
+            <NavLink
+              className={`${css.nav_link} ${false && css.visually_hidden}`}
+              to="favorites"
+            >
+              Favorites
+            </NavLink>
           </div>
-        )}
 
-        <div className={css.burger_menu_container}>
-          <BurgerMenuIcon />
+          <div className={css.auth_container}>
+            <button className={css.auth_btn}>LogIn</button>
+            <button className={css.auth_btn}>Registration</button>
+          </div>
+
+          {false && (
+            <div className={css.avatar_container}>
+              <div className={css.icon_container}>
+                <UserIcon />
+              </div>
+              <p className={css.user_title}>{'User'}</p>
+            </div>
+          )}
+
+          <div className={css.burger_menu_container}>
+            <BurgerMenuIcon />
+          </div>
         </div>
+        <div className={css.line}></div>
       </header>
       <main>
         <Outlet />
       </main>
-    </div>
+    </>
   );
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,50 @@
-// import { BackgroundRoundIcon } from 'assets/sprite';
+import { Link } from 'react-router-dom';
+import css from './HomePage.module.css';
+import image from '../../assets/home-image.png';
+import { CheckMarkIcon, PeopleIcon, QuestionMarkIcon } from 'assets/sprite';
 
 export const HomePage = () => {
   return (
-    <div className="container">
-      HomePage
-      {/* <BackgroundRoundIcon /> */}
+    <div className={`container ${css.home_page_container}`}>
+      <div className={css.campaign_container}>
+        <h1 className={css.main_title}>
+          The road to the <span className={css.part_of_title}>depths</span> of
+          the human soul
+        </h1>
+        <p className={css.description}>
+          We help you to reveal your potential, overcome challenges and find a
+          guide in your own life with the help of our experienced psychologists.
+        </p>
+
+        <div className={css.get_started_link_container}>
+          <Link className={css.get_started_link} to="psychologists">
+            Get Started
+          </Link>
+        </div>
+      </div>
+
+      <div className={css.image_container}>
+        <img className={css.image} src={image} alt={'girl'} />
+
+        <div className={css.question_mark_container}>
+          <QuestionMarkIcon />
+        </div>
+
+        <div className={css.people_container}>
+          <PeopleIcon />
+        </div>
+
+        <div className={css.experiance_container}>
+          <div className={css.check_mark_container}>
+            <CheckMarkIcon />
+          </div>
+
+          <div className={css.experiance_title_number_container}>
+            <p className={css.experiance_title}>Experienced psychologists</p>
+            <h3 className={css.expireance_number}>15,000</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
